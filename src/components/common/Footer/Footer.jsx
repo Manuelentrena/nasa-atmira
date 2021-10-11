14da932d19570cd5cb32d8f08547cf6255633188
Manuel Entrena <manuel.entrena@gmail.com>
2021-10-11 18:43:01 +0200

    Added styles in header and footer

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -3,17 +3,20 @@ import Logo from "../Logo/Logo";
 
 export default function Footer() {
   return (
-    <div className="footer">
-      <p className="footer__text">
-        Realizado por{" "}
-        <a
-          className="footer__link"
-          href="https://www.instagram.com/manuel_entrena"
-        >
-          @Manuel Entrena
-        </a>
-      </p>
-      <Logo />
-    </div>
+    <>
+      <div className="footer__header"></div>
+      <div className="footer">
+        <p className="footer__text">
+          Realizado por{" "}
+          <a
+            className="footer__link"
+            href="https://www.instagram.com/manuel_entrena"
+          >
+            @Manuel Entrena
+          </a>
+        </p>
+        <Logo />
+      </div>
+    </>
   );
 }
--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -2,6 +2,7 @@ export default function Header() {
   return (
     <div className="header">
       <h1 className="header__title">🪐 NASA-ATMIRA 🪐</h1>
+      <div className="header__footer"></div>
     </div>
   );
 }
