bb3863ad9d1c11b5fcef385db8c072026d0e429b
Manuel Entrena <manuel.entrena@gmail.com>
2021-10-11 20:07:23 +0200

    Added styles skeleton

--- src/components/common/Skeleton/LoadingDetail.tsx
+++ src/components/common/Skeleton/LoadingDetail.tsx
@@ -2,10 +2,17 @@ import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 export default function LoadingDetail() {
   return (
-    <SkeletonTheme color="#202020" highlightColor="#444">
-      <Skeleton className="skeleton" height={25} width={350} delay={1} />
-      <Skeleton className="skeleton" height={292} width={500} delay={1} />
-      <Skeleton className="skeleton" height={15} width={500} delay={1} />
-    </SkeletonTheme>
+    <div className="loadingDetail">
+      <SkeletonTheme color="#7a7a7a" highlightColor="#c8c8c8">
+        <div className="loadingDetail2">
+          <Skeleton className="skeleton" height={50} width={350} />
+          <Skeleton className="skeleton" height={250} width={500} />
+          <Skeleton className="skeleton" height={40} width={300} />
+          <Skeleton className="skeleton" height={400} width={500} />
+          <Skeleton className="skeleton" height={25} width={500} />
+          <Skeleton className="skeleton" height={25} width={500} />
+        </div>
+      </SkeletonTheme>
+    </div>
   );
 }
--- src/components/common/Skeleton/LoadingPlanet.tsx
+++ src/components/common/Skeleton/LoadingPlanet.tsx
@@ -2,11 +2,11 @@ import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 export default function LoadingPlanet() {
   return (
-    <SkeletonTheme color="#202020" highlightColor="#444">
+    <SkeletonTheme color="#7a7a7a" highlightColor="#c8c8c8">
       <div className="loadingPlanet">
-        <Skeleton className="skeleton" height={25} width={350} delay={1} />
-        <Skeleton className="skeleton" height={292} width={500} delay={1} />
-        <Skeleton className="skeleton" height={15} width={500} delay={1} />
+        <Skeleton className="skeleton" height={25} width={350} />
+        <Skeleton className="skeleton" height={292} width={500} />
+        <Skeleton className="skeleton" height={15} width={500} />
       </div>
     </SkeletonTheme>
   );
